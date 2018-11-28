cb3241d6645ddab5662aa0632de0b7f37aa05a17
rachmadza <razato.zai@gmail.com>
2018-11-28 11:04:14 +0700

    push anchor tic tac toe

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 //your code here
+function tic_tac_toe(row,col) {
+    let output = []
+    let X = 0
+    let O = 0
+    let XO = 'xo'
+    for (let i = 0; i < row; i++) {
+        let box = []
+        for (let j = 0; j < col; j++) {
+            let index = Math.floor(Math.random()*XO.length)
+            if((XO[index] == 'x' && X<5) || O>=5 ) {
+                box.push(XO[0])
+                X+=1
+            } else if ((XO[index] == 'o' && O<5) || X>=5) {
+                box.push(XO[1])
+                O+=1
+            } 
+            
+        }
+        output.push(box)
+    }
+
+    return output
+}
+
+console.log(tic_tac_toe(3,3));
