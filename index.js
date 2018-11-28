dc03694d7a1fe7aab6db84e056fa491f9c771c23
fauzan_bintang <obin.aji@gmail.com>
2018-11-28 10:16:43 +0700

    done

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-//your code here
+function ticTacToe() {
+    const size = 3
+    const xo = 'XO'
+
+    let countX = 0
+    let countO = 0
+    let result = []
+
+    for (let i = 0; i < size; i++) {
+        let temp = []
+        for (let j = 0; j < size; j++) {
+            temp[j] = xo[Math.floor(Math.random() * xo.length)]
+            if (countX === 5) {
+                temp[j] = 'O'
+            }
+            if (countO === 5) {
+                temp[j] = 'X'
+            }
+            if (temp[j] === 'X') {
+                countX++
+            } else {
+                countO++
+            }
+        }
+        result.push(temp)
+    }
+    return result
+}
+
+console.log(ticTacToe());
