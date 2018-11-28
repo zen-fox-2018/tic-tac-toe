2998c96f02e30e04e2d65316c2c169486f9f5258
patriagani <patria.gani@gmail.com>
2018-11-28 10:18:57 +0700

    tic tac toe done

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
-//your code here
+function tictactoe() {
+  let xo = "XO"
+  let array = []
+  let countX = 0
+  let countO = 0
+
+  for (let i = 0; i < 3; i++) {
+    let innerArray = []
+    for (let j = 0; j < 3; j++) {
+      let xoPush = xo[Math.floor(Math.random() * xo.length)]
+      if (countX === 5) {
+        innerArray.push("O")
+      }
+      else if (countO === 5) {
+        innerArray.push("X")
+      }
+      else {
+        if (xoPush === "X") {
+          countX++
+        }
+        else {
+          countO++
+        }
+        innerArray.push(xoPush)
+      }
+    }
+    array.push(innerArray)
+  }
+  return console.log(array);
+}
+
+tictactoe()
