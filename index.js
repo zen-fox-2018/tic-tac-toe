0e1acbd08cc00def38dc1118070327bc509bc715
Luthfi Arfiansyah <luthfi.arfi@gmail.com>
2018-11-28 11:03:02 +0700

    initial commit

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
 //your code here
+function ticTacToe (){
+  var xo = ['X', 'O'];
+  var result = [];
+  var indexX = 0;
+  var indexO = 0;
+  var isi ='';
+  for (var i = 0; i < 3; i++) {
+    var isiResult = [];
+    for (var j = 0; j < 3; j++) {
+      if (indexX <5 && indexO <5 ) {
+        isi = xo[Math.round(Math.random())];
+        if (isi == 'X'){
+          indexX++;
+        }
+        else {
+          indexO++;
+        }
+      }
+      else if (indexX === 4) {
+        isi = 'O';
+        indexO++;
+      }
+      else {
+        isi = 'X';
+        indexX;
+      }
+      isiResult.push(isi);
+    }
+    result.push(isiResult);
+  }
+  // console.log('X',indexX,'O',indexO);
+  return result;
+}
+
+console.log(ticTacToe());
