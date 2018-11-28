19e6dfb201d4debf061b7fb561d2a74221cd319b
ShadowM8 <anton2014lenovo@gmail.com>
2018-11-28 17:44:40 +0700

    edit n fix index.js

--- index.js
+++ index.js
@@ -1,32 +1,33 @@
 function tic_tac_toe() {
     let hasil = []
+    let counterX = 0
+    let counterO = 0
     for (let i = 0; i < 3; i++) {
         let hasilDalam = []
-        let counterX = 0
-        let counterO = 0
         for (let j = 0; j < 3; j++) {
-            let angka = Math.floor(Math.random() * (2-1+1) + 1)
-            if(angka === 2) {                
+            let angka = Math.floor(Math.random() * (2 - 1 + 1) + 1)
+            if (angka === 2) {
                 counterX++
-                if(counterX <=5){
-                    hasilDalam.push('O')
+                if (counterX <= 5) {
+                    hasilDalam.push(' O ')
                 }
                 else {
-                    hasilDalam.push('X')
+                    hasilDalam.push(' X ')
                 }
             }
             else {
                 counterO++
-                if(counterO <= 5){
-                    hasilDalam.push('X')
+                if (counterO <= 5) {
+                    hasilDalam.push(' X ')
                 }
                 else {
-                    hasilDalam.push('O')
+                    hasilDalam.push(' O ')
                 }
             }
         }
         hasil.push(hasilDalam)
     }
+    
     return hasil
 }
 
