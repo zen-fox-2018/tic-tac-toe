8f1a673db6543d5070bd7fb348251bdec7783883
ShadowM8 <anton2014lenovo@gmail.com>
2018-11-28 10:16:48 +0700

    tictactoe done -Anton-

--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-//your code here
+function tic_tac_toe() {
+    let hasil = []
+    for (let i = 0; i < 3; i++) {
+        let hasilDalam = []
+        let counterX = 0
+        let counterO = 0
+        for (let j = 0; j < 3; j++) {
+            let angka = Math.floor(Math.random() * (2-1+1) + 1)
+            if(angka === 2) {                
+                counterX++
+                if(counterX <=5){
+                    hasilDalam.push('O')
+                }
+                else {
+                    hasilDalam.push('X')
+                }
+            }
+            else {
+                counterO++
+                if(counterO <= 5){
+                    hasilDalam.push('X')
+                }
+                else {
+                    hasilDalam.push('O')
+                }
+            }
+        }
+        hasil.push(hasilDalam)
+    }
+    return hasil
+}
+
+console.log(tic_tac_toe())
