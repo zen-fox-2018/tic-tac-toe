c3c924b667d70df7ffbfd129579e2bd53f129be5
Zhang <zhang@ZhangMBP15.localdomain>
2018-11-28 18:32:38 +0700

    update

--- index.js
+++ index.js
@@ -35,5 +35,50 @@ function tictac() {
   }
   return result
 }
+// console.log(i())
 
-console.log(tictac())
+function cekMenang(board) {
+  let win = []
+  let counto = 0
+  let countx = 0 
+
+  board.forEach((arr, i) => {
+    arr.forEach((char , j) => {
+        if (arr[j+1] == char && arr[j+2] == char ){
+          win.push(char)
+        } 
+        if (board[i+1] !== undefined && board[i+2] !== undefined) {
+          if (board[i+1][j] == char && board[i+2][j] == char ) {
+            win.push(char)
+          }
+        }
+    })
+  })
+  if ((board[1][1] == board[0][0] && board[1][1] == board[2][2]) || (board[1][1] == board[0][2] && board[1][1] == board[2][0]) ) {
+    win.push(board[1][1])
+  }
+
+  win.forEach(x => {
+    if(x == 'O' ) {
+      counto++
+    } else {
+      countx++
+    }
+  })
+  // console.log(win)
+  if (counto > countx) {
+    return `pemenang adalah o`
+  } else if (countx > counto) {
+    return `pemenang adalah x`
+  } else if (counto == countx && counto !== 0) {
+    return `Seri`
+  } else {
+    return `tidak ad yg menang`
+  }
+
+}
+
+ 
+let board = tictac()
+console.log(board.join(' \n'))
+console.log(cekMenang(board))
