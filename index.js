9d2d686b76b9bc6c059cf5b8b7e0be1a4491539e
maria <msmariaulfa33@gmail.com>
2018-11-28 18:10:53 +0700

    revisi

--- index.js
+++ index.js
@@ -2,19 +2,25 @@
 function tictactoe (){
     let row = []
     let xo = ['X','O']
-    let countX = 5
-    for(let i = 0; i<3; i++){
+    let countX = 0
+    let countO = 0
+    for(let i = 0; i < 3; i++){
         let col = []
-        for(let j = 0; j<3; j++){
-            let k = Math.floor(Math.random()*2)
-            if(countX > 0){
-                col.push(xo[k])
-                if(xo[k] === 'X'){
-                    countX--
-                }
-            }else{
+        for(let j = 0; j < 3; j++){
+            if(countX === 4){
                 col.push('O')
-            } 
+            }else if(countO === 4){
+                col.push('X')
+            }else{
+                let k = Math.floor(Math.random()*2)
+                if(xo[k] === 'X'){
+                    countX++
+                    col.push('X')
+                }else{
+                    countO++
+                    col.push('O')
+                }
+            }
         }
         row.push(col)
     }
