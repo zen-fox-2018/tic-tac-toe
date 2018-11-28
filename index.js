d3b7975afd1d1aedd0903b5ac3b4f99dea86abf4
ChristianHS23 <christian.sihotang23@gmail.com>
2018-11-28 18:12:20 +0700

    add winner function

--- index.js
+++ index.js
@@ -34,5 +34,53 @@ function randomXO(){
     var random = ~~(Math.random()*2)
     return randomXOString[random]
 }
+function checkWin(board){
+    var Oscore=0
+    var Xscore=0
+    console.log(board)
+    for(let i=0; i < board.length; i++){
+        for(let j=0; j < board[i].length; j++){
+            if(board[i+1] && board[i+2]){
+                if(board[i][j] == 'x' && board[i+1][j] == 'x' && board[i+2][j]== 'x'){
+                    Xscore++
+                }
+                if(board[i][j] == 'o' && board[i+1][j] == 'o' && board[i+2][j]== 'o'){
+                    Oscore++
+                }
+            }
+            if(board[j+1] && board[j+2]){
+                if(board[i][j] == 'x' && board[i][j+1] == 'x' && board[i][j+2] == 'x'){
+                    Xscore++
+                }
+                if(board[i][j] == 'o' && board[i][j+1] == 'o' && board[i][j+2] == 'o'){
+                    Oscore++
+                }
+            }
+            if(i == 1 && j == 1){
+                if(board[i-1][j-1] == 'x' && board[i+1][j+1] == 'x' && board[i][j] == 'x' ){
+                    Xscore++
+                }
+                if(board[i-1][j-1] == 'o' && board[i+1][j+1] == 'o' && board[i][j] == 'o' ){
+                    Oscore++
+                }
+                if(board[i-1][j+1] == 'x' && board[i+1][j-1] == 'x' && board[i][j] == 'x'){
+                    Xscore++
+                }
+                if(board[i-1][j+1] == 'o' && board[i+1][j-1] == 'o' && board[i][j] == 'o'){
+                    Oscore++
+                }
+            }
+        }
+    }
+    console.log(Xscore,Oscore)
+    if(Xscore > Oscore)
+        return "X Wins"
+    else if(Oscore > Xscore)
+        return "O Wins"
+    else
+        return "Seri"
+   
+}
 
-console.log(tictactoe(3,3))
+//console.log(tictactoe(3,3))
+console.log(checkWin(tictactoe(3,3)))
