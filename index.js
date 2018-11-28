a48ed304ef62ff502c7d845c4d3bb914ddc8ca71
Celyn Susanto <celyn.vy@gmail.com>
2018-11-28 17:28:50 +0700

    error fixed

--- index.js
+++ index.js
@@ -7,16 +7,20 @@ function ticTacToe () {
     for (let i = 0; i < 3; i++) {
         var temp = [];
         for (let j = 0; j < 3; j++) {
-            let index = Math.round (Math.random() * (1) + 0);
+            let index = Math.round (Math.random() * (1-0));
             if (xAndO[index] == 'x') {
                 counterX++;
                 if (counterX <= 5) {
                     temp.push(xAndO[index]);
+                } else {
+                    temp.push("o");
                 }
             } else {
                 counterO++
                 if (counterO <= 5) {
                     temp.push(xAndO[index]);
+                } else {
+                    temp.push("x")
                 }
             }
         }
